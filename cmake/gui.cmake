43ed82a049f30874da8c32f306811923afb6fc46
Dmitry Ponyatov <dponyatov@gmail.com>
2026-08-04 13:16:18 +0400

    .

--- cmake/gui.cmake
+++ cmake/gui.cmake
@@ -6,12 +6,12 @@ find_package(SDL2 REQUIRED)
 
 if(SDL2_FOUND)
   message("-- Found SDL2: ${SDL2_LIBRARIES} (found version \"${SDL2_VERSION}\")")
-  add_compile_definitions(SDL2_FOUND)
-  add_compile_definitions(SDL)
+  add_compile_definitions(SDL SDL2_FOUND)
 
 #   add_library(SDL2::SDL2 INTERFACE IMPORTED)    
 
   target_include_directories(SDL2::SDL2 INTERFACE ${SDL2_INCLUDE_DIRS})
   target_compile_options(SDL2::SDL2 INTERFACE ${SDL2_CFLAGS_OTHER})
   target_link_libraries(SDL2::SDL2 INTERFACE ${SDL2_LIBRARIES})
+  list(APPEND L SDL2::SDL2)
 endif()
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,9 @@
 
 extern int main(int argc, char *argv[]) {
     arg(0, argv[0]);
+#ifdef SDL
+    assert(!SDL_Init(SDL_INIT_VIDEO));
+#endif                                // SDL
     for (int i = 1; i < argc; i++) {  //
         arg(i, argv[i]);
         yyfile = argv[i];
